fec75eb62298d6c7c9acffd9b1e48632dbb3186f
HAKN1999 <hafizdkurniawan700@gmail.com>
2022-10-19 10:21:00 +0700

    feat: add response status

---
+++ helper/response.go
@@ -0,0 +1,50 @@
+package helper
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Payload interface{} `json:"payload,omitempty"`
+	Error   interface{} `json:"error,omitempty"`
+}
+
+func WriteJsonRespnse(ctx *gin.Context, resp *Response) {
+	ctx.JSON(resp.Status, resp)
+}
+
+func SuccessCreateResponse(payload interface{}, message string) *Response {
+	return &Response{
+		Status:  http.StatusCreated,
+		Message: message,
+		Payload: payload,
+	}
+}
+
+func SuccessResponse(payload interface{}, message string) *Response {
+	return &Response{
+		Status:  http.StatusOK,
+		Message: message,
+		Payload: payload,
+	}
+}
+
+func InternalServerError(err error) *Response {
+	return &Response{
+		Status:  http.StatusInternalServerError,
+		Message: "Internal server error",
+		Error:   err.Error(),
+	}
+}
+
+func BadRequestResponse(err error) *Response {
+	return &Response{
+		Status:  http.StatusBadRequest,
+		Message: "Bad request",
+		Error:   err.Error(),
+	}
+}
