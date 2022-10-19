254904daf976b2b240cf28e5e978e85f404b2970
HAKN1999 <hafizdkurniawan700@gmail.com>
2022-10-19 16:42:40 +0700

    feat: add param todo update

--- todo/input.go
+++ todo/input.go
@@ -3,3 +3,8 @@ package todo
 type TodoInput struct {
 	Task string `json:"task" binding:"required"`
 }
+
+type TodoUpdateInput struct {
+	Task      string `json:"task" binding:"required"`
+	Completed bool   `json:"completed" binding:"required"`
+}
